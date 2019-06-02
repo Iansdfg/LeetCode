cdd1fe9f683dde29bdb3adc24a81f82cb7eae9dc
Iansdfg <47009085+Iansdfg@users.noreply.github.com>
2019-06-01 22:27:02 -0700

    Create 9_Palindrome_Number.py

---
+++ class Solution(object):     def isPalindrome(self, x):         """         :type x: int         :rtype: bool         """         tar = x         if x < 0:             return False         result = 0         while x > 0:             result = result*10 + x % 10             x = int(x/9_Palindrome_Number.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        tar = x
+        if x < 0:
+            return False
+        result = 0
+        while x > 0:
+            result = result*10 + x % 10
+            x = int(x / 10)
+        if result == tar:
+            return True
+
+
+def execute():
+    sol = Solution()
+    s = 121
+    print(sol.isPalindrome(s))
+
+if __name__ == '__main__':
+    execute()
+
