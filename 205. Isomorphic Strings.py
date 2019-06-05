d688db0e04d0cc4c9bc298c0c33d487b393793cc
Iansdfg <47009085+Iansdfg@users.noreply.github.com>
2019-06-04 22:35:29 -0700

    Update 205. Isomorphic Strings.py

--- 205. Isomorphic Strings.py
+++ 205. Isomorphic Strings.py
@@ -5,14 +5,26 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
+        # hashmap = {}
+        # for i in range(len(s)):
+        #     if s[i] not in hashmap:
+        #         hashmap[s[i]] = t[i]
+        #     elif hashmap[s[i]] != t[i]:
+        #         return False
+        # mapval = [hashmap[k] for k in hashmap]
+        # return len(mapval) == len(set(mapval))
         hashmap = {}
+        valmap = {}
         for i in range(len(s)):
-            if s[i] not in hashmap:
-                hashmap[s[i]] = t[i]
-            elif hashmap[s[i]] != t[i]:
+            if s[i] in hashmap:
+                if hashmap[s[i] ]!= t[i]:
+                    return False
+            elif t[i] in valmap:
                 return False
-        mapval = [hashmap[k] for k in hashmap]
-        return len(mapval) == len(set(mapval))
+            else:
+                hashmap[s[i]]=t[i]
+                valmap[t[i]]=True
+        return True
 
 
 def execute():
