63f549fc375a1573473f2a57c93480165e6c710e
Iansdfg <47009085+Iansdfg@users.noreply.github.com>
2019-06-06 21:51:31 -0700

    Update 34. Find First and Last Position of Element in Sorted Array.py

--- 34. Find First and Last Position of Element in Sorted Array.py
+++ 34. Find First and Last Position of Element in Sorted Array.py
@@ -1,44 +1,66 @@
 class Solution(object):
-    def searchInsert(self, nums, target):
+    def searchRange(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
-        :rtype: int
+        :rtype: List[int]
         """
-        lo, hi = 0, len(nums)-1
-        while lo < hi:
-            mid = (lo + hi) / 2
-            if nums[mid] >= target:
-                hi = mid
+        low, high = 0, len(nums)-1
+        while low <= high:
+            mid = (low+high)//2
+            if nums[mid] == target:
+                L = R = mid
+                while L>=low and nums[L]==target:
+                    L-=1
+                while R<=high and nums[R]==target:
+                    R+=1
+                return [L+1,R-1]
+            if nums[low]<=target<nums[mid]:
+                high = mid-1
             else:
-                lo = mid + 1
-        if lo == len(nums) - 1 and nums[lo] < target: lo += 1
-        if len(nums) == 1 and nums[0] >= target: lo = 0
-        return lo
-        # insert = 0
-        # for index, value in enumerate(nums):
-        #     if value == target:
-        #         return index
-        #     if value < target:
-        #         insert += 1
-        # return insert
+                low = mid+1
+        return [-1,-1]
+        # left, right = 0, len(nums)-1
+        # if len(nums) == 0:
+        #     return [-1, -1]
+        # find = None
+        # if nums[0] <= target and nums[-1] >= target:
+        #     while left<=right:
+        #         mid = (left+right)>>1
+        #         if nums[mid] == target:
+        #             find = mid
+        #             break
+        #         elif nums[mid] > target:
+        #             right = mid-1
+        #         elif nums[mid] < target:
+        #             left = mid+1
+        #     if find == None:
+        #         return [-1,-1]
+        #     # print(find)
+        #     first = last = find
+        #     while first-1 >= 0 and nums[first-1] == nums[find]:
+        #         first -= 1
+        #     while last+1 < len(nums) and nums[last+1] == nums[find]:
+        #         last += 1
+        #     # print(nums[first+1], nums[last-1], first+1, last-1)
+        #     return [first, last]
+        # return [-1, -1]
+
 
 
 
 def execute():
     sol = Solution()
-
-    # digits = [1, 3, 5, 6]
-    # target = 5
-    digits = [1, 3, 5, 6]
-    target = 2
-    # digits = [1, 3, 5, 6]
-    # target = 7
-    digits = []
-    target = 0
-
-    print(sol.searchInsert(digits,target))
+    nums = [5, 7, 7, 8, 8, 10]
+    target = 8
+    # nums = [5, 7, 7, 8, 8, 10]
+    # target = 6
+    nums = [1,1]
+    target = 1
+    print(sol.searchRange(nums, target))
 
 if __name__ == '__main__':
     execute()
+
+
 
