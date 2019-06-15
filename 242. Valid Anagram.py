1f549c1e4ca8e01694ddd38c6c4349ab0c72e9b5
Iansdfg <47009085+Iansdfg@users.noreply.github.com>
2019-06-14 18:19:25 -0700

    Update 242. Valid Anagram.py

--- 242. Valid Anagram.py
+++ 242. Valid Anagram.py
@@ -5,19 +5,25 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        dic = {}
-        for char in s:
-            if char in dic:
-                dic[char]+=1
-            else:
-                dic[char] = 1
-        for char in t:
-            if char in dic:
-                dic[char]-=1
-            else:
-                return False
-        for key in dic:
-            if dic[key]!= 0:
+#         dic = {}
+#         for char in s:
+#             if char in dic:
+#                 dic[char]+=1
+#             else:
+#                 dic[char] = 1
+#         for char in t:
+#             if char in dic:
+#                 dic[char]-=1
+#             else:
+#                 return False
+#         for key in dic:
+#             if dic[key]!= 0:
+#                 return False
+#         return True
+        if len(set(s))!=len(set(t)):
+            return False
+        for i in set(s):
+            if s.count(i)!=t.count(i):
                 return False
         return True
         
